46e688a585da5f998ffa40e826413f8f62396884
Jacob Ray Gilman <jacob@oscillas.com>
2024-06-02 10:20:46 -0500

    Refactor decoder logic to reduce duplication

--- src/cpu.c
+++ src/cpu.c
@@ -1,7 +1,5 @@
 #include "../includes/cpu.h"
 
-#include "stdio.h"
-
 void execute_instruction(
     uint16_t instruction,
     stack * stack,
@@ -10,6 +8,8 @@ void execute_instruction(
     frame_buffer * frameBuffer,
     v_registers * vRegisters
 ) {
+    uint16_t instructionTopNibbleOnly = (instruction & 0xF000);
+
     if (instruction == 0x00E0)
     {
         clearFrameBuffer(frameBuffer);
@@ -19,17 +19,17 @@ void execute_instruction(
         (*programCounter) = (*stack)[(*stackPointer) - 1];
         (*stackPointer)--;
     }
-    else if ((instruction & 0xF000) == 0x1000)
+    else if (instructionTopNibbleOnly == 0x1000)
     {
         (*programCounter) = instruction & 0x0FFF;
     }
-    else if ((instruction & 0xF000) == 0x2000)
+    else if (instructionTopNibbleOnly == 0x2000)
     {
         (*stackPointer)++;
         (*stack)[(*stackPointer) - 1] = *programCounter;
         (*programCounter) = instruction & 0x0FFF;
     }
-    else if ((instruction & 0xF000) == 0x3000)
+    else if (instructionTopNibbleOnly == 0x3000)
     {
         // SE VX, NN (3XNN)
         uint8_t valueToCompare = instruction & 0xFF;
@@ -40,7 +40,7 @@ void execute_instruction(
             (*programCounter) += 2;
         }
     }
-    else if ((instruction & 0xF000) == 0x4000)
+    else if (instructionTopNibbleOnly == 0x4000)
     {
         // SNE VX, NN (4XNN)
         uint8_t valueToCompare = instruction & 0xFF;
@@ -51,7 +51,7 @@ void execute_instruction(
             (*programCounter) += 2;
         }
     }
-    else if ((instruction & 0xF000) == 0x5000)
+    else if (instructionTopNibbleOnly == 0x5000)
     {
         uint8_t vRegisterOne = (instruction >> 4) & 0xF;
         uint8_t vRegisterTwo = (instruction >> 8) & 0xF;
